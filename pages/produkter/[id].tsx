98be112696a8e0b5ec0822bc8144e7ca28618606
Robert <robertwattboolsen@gmail.com>
2021-11-20 19:09:55 +0100

    Creating primary image on produkt page

--- pages/produkter/[id].tsx
+++ pages/produkter/[id].tsx
@@ -1,67 +1,81 @@
-import { createClient, Entry } from 'contentful';
-import { GetStaticPaths, GetStaticProps } from 'next'
-import { useRouter } from 'next/dist/client/router';
-import MainLayout from '../../components/MainLayout';
-import Product from '../../types/Product';
+import { createClient, Entry } from "contentful";
+import { GetStaticPaths, GetStaticProps } from "next";
+import { useRouter } from "next/dist/client/router";
+import MainLayout from "../../components/MainLayout";
+import Product from "../../types/Product";
+import Image from "next/image";
 
 interface ProductProps {
-    product: Entry<Product>    
+  product: Entry<Product>;
 }
- 
+
 const Product: React.FunctionComponent<ProductProps> = ({ product }) => {
+  const router = useRouter();
 
-    const router = useRouter()
+  console.log(product);
 
-    console.log(product);
-    
-    return router.isFallback ? ( 
-        <h1>loading...</h1>
-     ) : (
-      <MainLayout>
-         <h1>Everything is fine</h1>
-      </MainLayout>   
-     )
-}
+  return router.isFallback ? (
+    <h1>loading...</h1>
+  ) : (
+    <MainLayout>
+      <div className="relative w-full h-[400px] bg-gray-200 rounded-lg overflow-hidden">
+        <Image
+          src={"https:" + product.fields.billede.fields.file.url}
+          alt={product.fields.billede.fields.title}
+          layout="fill"
+          quality="10"
+          className="object-center object-cover"
+        />
+        <div className="relative m-auto w-96 h-96 bg-gray-200 rounded-lg overflow-hidden">
+          <Image
+            src={"https:" + product.fields.billede.fields.file.url}
+            alt={product.fields.billede.fields.title}
+            layout="fill"
+            quality="10"
+            className="object-center object-cover"
+          />
+        </div>
+      </div>
+    </MainLayout>
+  );
+};
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_TOKEN,
+  });
 
-    const client = createClient({
-        space: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_TOKEN,
-      });
-    
-      const contentfulEntires = await client.getEntries();
+  const contentfulEntires = await client.getEntries();
 
-      const products = contentfulEntires.items.filter(
-        item => item.sys.contentType.sys.id === "product"
-      );
-      
-      const paths = products.map(product => ({
-          params: { id: product.sys.id }
-      }))
-      
-      return {
-        paths,
-        fallback: true
-      };
-}
+  const products = contentfulEntires.items.filter(
+    (item) => item.sys.contentType.sys.id === "product"
+  );
+
+  const paths = products.map((product) => ({
+    params: { id: product.sys.id },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+};
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_TOKEN,
+  });
 
-    const client = createClient({
-      space: process.env.CONTENTFUL_SPACE_ID,
-      accessToken: process.env.CONTENTFUL_TOKEN,
-    });
-  
-    const product = await client.getEntry(params?.id);
-  
-    return {
-      props: {
-        product
-      },
-      revalidate: 10
-    };
+  const product = await client.getEntry(params?.id);
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 10,
   };
-  
- 
+};
+
 export default Product;
