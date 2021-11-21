2c15b045a19d3f682ce5e66c0138b2efff92bcde
Robert <u1501851@rts-365.dk>
2021-11-21 13:33:13 +0100

    Added Tailwind JIT

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  mode: 'jit',
-  purge: [],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
