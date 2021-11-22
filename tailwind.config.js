b62bf99a39f6bbebb029409957e833c760717da0
Robert <robertwattboolsen@gmail.com>
2021-11-22 21:41:03 +0100

    Added xsm breakpoint to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      screens: {
+        'xsm': '400px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
       colors: {
         primary: '#8B5CF6'
       }
