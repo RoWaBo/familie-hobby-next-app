b001dd2fe2ea6c5bf9c556a5d0c845d62d039b28
Robert <robertwattboolsen@gmail.com>
2021-11-23 12:15:04 +0100

    Added header til Forside

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,22 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
+import MainLayout from "../components/MainLayout";
 
 const Home: NextPage = () => {
   return (
-    <>
-      <h1>Forside</h1>
-    </>
-  )
-}
+    <MainLayout>
+      <header>
+        <div>
+          <h1 className="text-2xl sm:text-4xl text-center leading-9 sm:leading-[3.5rem]">
+            Velkommen til <br /> Kreativ Familie Hobby
+          </h1>
+          <p className="text-base sm:text-lg text-center mt-4">
+            Her kan du få inspiration til kreative projekter og se vores udvalg af
+            spændende produkter!
+          </p>
+        </div>
+      </header>
+    </MainLayout>
+  );
+};
 
-export default Home
+export default Home;
