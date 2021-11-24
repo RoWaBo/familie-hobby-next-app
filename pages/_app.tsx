87cd7cbd2502cfcac14bf4745f8423d5ff8bb407
Robert <robertwattboolsen@gmail.com>
2021-11-24 15:13:22 +0100

    Added Head to App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import NavBar from '../components/NavBar'
+import Head from '../components/Head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return ( 
     <>
+      <Head />
       <NavBar />
       <Component {...pageProps} />
     </>
