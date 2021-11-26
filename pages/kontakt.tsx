c241c2d811aa7d125fda994bcbdb7713ccdbba53
Robert <robertwattboolsen@gmail.com>
2021-11-26 10:11:34 +0100

    Removed flex desgin in Kontakt

--- pages/kontakt.tsx
+++ pages/kontakt.tsx
@@ -1,14 +1,12 @@
 import MainLayout from "../components/MainLayout";
 import { PhoneIcon, MailIcon } from "@heroicons/react/outline";
 
-interface ContactProps {
-
-}
+interface ContactProps {}
 
 const Contact: React.FunctionComponent<ContactProps> = () => {
-    return (
-        <MainLayout>
-            <article className="">
+  return (
+    <MainLayout>
+      {/* <article className="">
                 <h1 className="text-2xl sm:text-4xl leading-9 sm:leading-[3.5rem] font-normal">
                     <span className="text-purple-500 font-medium">
                         Kontakt
@@ -31,9 +29,29 @@ const Contact: React.FunctionComponent<ContactProps> = () => {
                         </li>
                     </ul>
                 </div>
-            </article>
-        </MainLayout>
-    );
-}
+            </article> */}
+      <article className="max-w-lg m-auto">
+        <h1 className="text-2xl sm:text-4xl leading-9 sm:leading-[3.5rem] font-normal">
+          <span className="text-purple-500 font-medium">Kontakt</span>
+        </h1>
+        <p className="text-base sm:text-lg mt-4 md:mt-6 flex-1 md:mr-8">
+          Hvis du har ris eller ros, skøre ideer og vanvittigt indfald, så tag endelig kontakt!
+        </p>
+        <ul className="text-base sm:text-lg text-gray-600 mt-6 flex-1">
+          <li className="flex items-center">
+            <span className="sr-only">mobil nummer:</span>
+            <PhoneIcon height={20} width={20} className="mr-4" />
+            93 83 80 23
+          </li>
+          <li className="flex items-center mt-3">
+            <span className="sr-only">Mail:</span>
+            <MailIcon height={20} width={20} className="mr-4" />
+            mathilde@kreativfamiliehobby.dk
+          </li>
+        </ul>
+      </article>
+    </MainLayout>
+  );
+};
 
 export default Contact;
