592c1dc401a75f27a9e97c03139765db5d200637
Robert <u1501851@rts-365.dk>
2021-12-02 15:27:44 +0100

    Created correct semantic for list in Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,26 +4,26 @@ const Footer: React.FC = () => {
   return (
     <footer className="w-fit m-auto sm:w-full px-4 py-8 sm:py-10 sm:px-8 lg:max-w-7xl lg:mx-auto lg:px-8">
       <ul className="text-xs text-gray-400 flex flex-col sm:justify-around sm:flex-row">
-        <div className="mr-8 md:mr-[10%]">
-          <li className="flex items-start">
+        <li className="mr-8 md:mr-[10%]">
+          <div className="flex items-start">
             <UserCircleIcon height={15} width={15} className="mr-2" />
             Mathilde Mei Jørgensen
-          </li>
-          <li className="flex items-start mt-3">
+          </div>
+          <div className="flex items-start mt-3">
             <IdentificationIcon height={15} width={15} className="mr-2" />
             CVR 30640055
-          </li>
-        </div>
-        <div>
-          <li className="flex items-start mt-3 sm:mt-0">
+          </div>
+        </li>
+        <li>
+          <div className="flex items-start mt-3 sm:mt-0">
             <HomeIcon height={15} width={15} className="mr-2" />
             Kreativ Familiehobby
-          </li>
-          <li className="flex items-start mt-3">
+          </div>
+          <div className="flex items-start mt-3">
             <GlobeIcon height={15} width={15} className="mr-2" />
             I det naturskønne Kattinge, Lejre Kommune
-          </li>
-        </div>
+          </div>
+        </li>
       </ul>
     </footer>
   );
