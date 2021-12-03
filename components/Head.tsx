0521724a0b1c2895a3983ff0a657cbabf50e3af4
Robert <u1501851@rts-365.dk>
2021-12-03 13:36:38 +0100

    Added language to Head

--- components/Head.tsx
+++ components/Head.tsx
@@ -3,9 +3,10 @@ import { default as NextHead } from 'next/head'
 const Head: React.FC = () => {
     return (
         <NextHead>
+            <html lang="da" />
             <title>Kreativ Familiehobby</title>
             <link rel="logo" href={"/scissorIcon.svg"} />
-            <meta content="Få inspiration til hyggelige og kreative projekter/produkter. Egnet til at hele familien kan være med." name="description" />
+            <meta content="Få inspiration til at skabe hyggelige og kreative projekter med hele familien. Vi har også en kollektion af hjemmelavede produkter. " name="description" />
         </NextHead>
     );
 }
