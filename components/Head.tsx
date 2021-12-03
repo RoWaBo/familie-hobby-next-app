6f6b7ea012bbe1ffcdc1e6eb06689104d1f8002d
Robert <u1501851@rts-365.dk>
2021-12-03 13:24:27 +0100

    Added name to meta tag'

--- components/Head.tsx
+++ components/Head.tsx
@@ -1,13 +1,13 @@
 import { default as NextHead } from 'next/head'
 
 const Head: React.FC = () => {
-    return ( 
+    return (
         <NextHead>
             <title>Kreativ Familiehobby</title>
             <link rel="logo" href={"/scissorIcon.svg"} />
-            <meta content="få inspiration til hyggelige og kreative projekter/produkter" />   
+            <meta content="Få inspiration til hyggelige og kreative projekter/produkter. Egnet til at hele familien kan være med." name="description" />
         </NextHead>
-     );
+    );
 }
- 
+
 export default Head;
